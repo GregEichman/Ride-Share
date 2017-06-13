30803e286496f5bcd8bf0041142323b2060db878
3120weissee <jackweisse@gmail.com>
2017-06-13 14:39:17 -0500

    firebase changes

--- src/App.js
+++ src/App.js
@@ -5,41 +5,41 @@ import Login from "./login";
 import List from "./list";
 import firebase from "firebase";
 
-const jackRide = "Jack Weisse2017-06-15 09:30:00";
-const bradRide = "Brad Aleckson2017-06-16 13:30:00";
-const initUsers = {
-  jackweisse: {
-    name: "Jack Weisse",
-    email: "jweisse@nm.com",
-    phone: "123-456-7890"
-  },
-  bradaleckson: {
-    name: "Brad Aleckson",
-    email: "baleckson@nm.com",
-    phone: "976-534-2109"
-  }
-}; 
+// const jackRide = "Jack Weisse2017-06-15 09:30:00";
+// const bradRide = "Brad Aleckson2017-06-16 13:30:00";
+// const initUsers = {
+//   jackweisse: {
+//     name: "Jack Weisse",
+//     email: "jweisse@nm.com",
+//     phone: "123-456-7890"
+//   },
+//   bradaleckson: {
+//     name: "Brad Aleckson",
+//     email: "baleckson@nm.com",
+//     phone: "976-534-2109"
+//   }
+// }; 
 
-const initRides = {
-  [jackRide]: {
-    "id": "Jack Weisse2017-06-15 09:30:00",
-    "driver": "Jack Weisse",
-    "date": "2017-06-15 09:30:00",
-    "capacity": 4,
-    "direction": "F_to_DT",
-    "notes": "Note for Jack's ride",
-    "passengers": []
-  },
-  [bradRide]: {
-    "id": "Brad Aleckson2017-06-16 13:30:00",
-    "driver": "Brad Aleckson",
-    "date": "2017-06-16 13:30:00",
-    "capacity": 6,
-    "direction": "F_to_DT",
-    "notes": "Note for Brad's ride.  He has a sweet minivan, 13 years old and >201,000 miles!",
-    "passengers": []
-  }
-}
+// const initRides = {
+//   [jackRide]: {
+//     "id": "Jack Weisse2017-06-15 09:30:00",
+//     "driver": "Jack Weisse",
+//     "date": "2017-06-15 09:30:00",
+//     "capacity": 4,
+//     "direction": "F_to_DT",
+//     "notes": "Note for Jack's ride",
+//     "passengers": []
+//   },
+//   [bradRide]: {
+//     "id": "Brad Aleckson2017-06-16 13:30:00",
+//     "driver": "Brad Aleckson",
+//     "date": "2017-06-16 13:30:00",
+//     "capacity": 6,
+//     "direction": "F_to_DT",
+//     "notes": "Note for Brad's ride.  He has a sweet minivan, 13 years old and >201,000 miles!",
+//     "passengers": []
+//   }
+// }
 
 
 class App extends Component {
@@ -57,8 +57,8 @@ class App extends Component {
 
   componentDidMount() {
     const database = firebase.database();
-    database.ref("riders").set(initRides);
-    database.ref("users").set(initUsers);
+    // database.ref("riders").set(initRides);
+    // database.ref("users").set(initUsers);
     database.ref("riders").once("value").then((snapshot)=>{
       console.log(snapshot.val());
     }).catch(console.log); 
