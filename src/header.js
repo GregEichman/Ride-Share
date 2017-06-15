4ef1d83f6048697c30016bef37028d267ab27edd
3120weissee <jackweisse@gmail.com>
2017-06-15 08:00:00 -0500

    merging with homework

--- src/header.js
+++ src/header.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
 const Header = (props) => {
-    
-   const loggedInUser = () => {
-            if(props.user)
-                return <div>logged in as: {props.user.name}</div>
-        }
+
+    const loggedInUser = (props.user.name ? (<div>logged in as: {props.user.name}</div>) : (<div></div>));
+
     return (
         <div style={{width:"40%",margin:"auto auto", marginTop:"30px",textAlign:"center"}}>
             <h1>NM Ride Share</h1>
-            {loggedInUser()}
+            {loggedInUser}
             <hr/>
         </div>
     );
